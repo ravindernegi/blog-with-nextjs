84af0fa073aa655bd906ac315bde837b9ddb3cb4
Ravinder Negi <r.singh.negi85@gmail.com>
2022-03-11 20:20:24 +0530

    Fixed updated

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { MainLayout } from '../components/layout';
+import { DefaultLayout } from '../components/layout';
 import Header from '../components/header';
 
 export default function About() {
   return <div className='md:container md:mx-auto'>About</div>;
 }
-
-About.getLayout = function getLayout(page) {
-  return <MainLayout>{page}</MainLayout>;
-};
